160cc38227ec8cc64448b35fe0e3498869763c88
hootnot <f.brekeveld@gmail.com>
2016-07-20 16:44:06 +0200

    decorator function added for dynamic insertion of documentation

--- oandapyV20/endpoints/apirequest.py
+++ oandapyV20/endpoints/apirequest.py
@@ -1,6 +1,34 @@
 """Handling of API requests."""
 
 
+def dyndoc_insert(src):
+    """docstring_insert - a decorator to insert API-docparts dynamically."""
+    # manipulating docstrings this way is tricky due to indentation
+    # the JSON needs leading whitespace to be interpreted correctly
+    import json
+
+    def mkblock(d):
+        # response, pretty formatted
+        v = json.dumps(d, indent=2)
+        # add leading whitespace for each line and start with a newline
+        return "\n{}".format("".join(["{0:>16}{1}\n".format("", L)
+                             for L in v.split('\n')]))
+
+    def dec(obj):
+        sub = {}
+        for k in src.keys():
+            # url
+            sub["{}_url".format(k)] = src[k]["url"]
+            sub["{}_resp".format(k)] = mkblock(src[k]["response"])
+            if "body" in src[k]:
+                sub["{}_body".format(k)] = mkblock(src[k]["body"])
+
+        obj.__doc__ = obj.__doc__.format(**sub)
+        return obj
+
+    return dec
+
+
 class APIRequest(object):
     """Base Class for API-request classes."""
 

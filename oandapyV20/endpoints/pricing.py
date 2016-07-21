6a7fe40e9964b00fb54464744c2611da1f039565
hootnot <f.brekeveld@gmail.com>
2016-07-21 12:59:21 +0200

    minor code modifications and docstring

--- oandapyV20/endpoints/pricing.py
+++ oandapyV20/endpoints/pricing.py
@@ -5,18 +5,16 @@ from .apirequest import APIRequest
 class Pricing(APIRequest):
     """Pricing - class to handle pricing endpoint."""
 
-    ENDPOINT = "/v3/accounts/{}/pricing"
+    ENDPOINT = "/v3/accounts/{accountID}/pricing"
 
-    def __init__(self, account_id):
+    def __init__(self, accountID):
         """Instantiate a Pricing APIRequest instance.
 
         Parameters
         ----------
-        account_id : string (optional)
-            the account_id of the account. Optional when requesting
-            all accounts. For all other requests to endpoint it is
-            required.
+        accountID : string (required)
+            the accountID of the account. 
         """
-        endpoint = self.ENDPOINT.format(account_id)
+        endpoint = self.ENDPOINT.format(accountID=accountID)
 
         super(Pricing, self).__init__(endpoint)

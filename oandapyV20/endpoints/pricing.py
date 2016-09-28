3121d159e2184359545fb502223d56f1047449f5
hootnot <f.brekeveld@gmail.com>
2016-09-28 17:42:19 +0200

    decorator based handling of pricing, each endpoint it's own class

--- oandapyV20/endpoints/pricing.py
+++ oandapyV20/endpoints/pricing.py
@@ -1,20 +1,34 @@
 """Handle pricing endpoints."""
 from .apirequest import APIRequest
+from .decorators import dyndoc_insert, endpoint, abstractclass
+
+responses = {}
 
 
+@abstractclass
 class Pricing(APIRequest):
     """Pricing - class to handle pricing endpoint."""
 
-    ENDPOINT = "v3/accounts/{accountID}/pricing"
+    ENDPOINT = ""
+    METHOD = "GET"
 
+    @dyndoc_insert(responses)
     def __init__(self, accountID):
         """Instantiate a Pricing APIRequest instance.
 
         Parameters
         ----------
         accountID : string (required)
-            the accountID of the account. 
+            the accountID of the account.
         """
         endpoint = self.ENDPOINT.format(accountID=accountID)
+        super(Pricing, self).__init__(endpoint, method=self.METHOD)
 
-        super(Pricing, self).__init__(endpoint)
+
+@endpoint("v3/accounts/{accountID}/pricing")
+class PricingInfo(Pricing):
+    """Pricing.
+
+    Get pricing information for a specified list of Instruments within
+    an account.
+    """

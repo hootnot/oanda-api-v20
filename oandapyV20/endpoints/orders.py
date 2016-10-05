7ffc5d6bf308d9f6e269acef5fe1a439e7bb989a
hootnot <f.brekeveld@gmail.com>
2016-10-05 12:36:55 +0200

    response added for order list

--- oandapyV20/endpoints/orders.py
+++ oandapyV20/endpoints/orders.py
@@ -3,7 +3,33 @@ from .apirequest import APIRequest
 from .decorators import dyndoc_insert, endpoint, abstractclass, params
 
 # responses serve both testing purpose aswell as dynamic docstring replacement
-responses = {}
+responses = {
+    "_v3_accounts_accountID_orders": {
+        "url": "v3/accounts/{accountID}/orders",
+        "response": {
+            "orders": [
+                {
+                    "triggerCondition": "TRIGGER_DEFAULT",
+                    "partialFill": "DEFAULT_FILL",
+                    "price": "1.20000",
+                    "stopLossOnFill": {
+                        "timeInForce": "GTC",
+                        "price": "1.22000"
+                    },
+                    "createTime": "2016-10-05T10:25:47.627003645Z",
+                    "timeInForce": "GTC",
+                    "instrument": "EUR_USD",
+                    "state": "PENDING",
+                    "units": "-100",
+                    "id": "2125",
+                    "type": "LIMIT",
+                    "positionFill": "POSITION_DEFAULT"
+                }
+            ],
+            "lastTransactionID": "2129"
+        }
+    }
+}
 
 
 @abstractclass

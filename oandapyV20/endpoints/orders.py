75042b68e2f516e949c859ddee20b6cb03d8dbbf
hootnot <f.brekeveld@gmail.com>
2016-10-05 13:17:03 +0200

    response added for order replace call

--- oandapyV20/endpoints/orders.py
+++ oandapyV20/endpoints/orders.py
@@ -28,6 +28,28 @@ responses = {
             ],
             "lastTransactionID": "2129"
         }
+    },
+    "_v3_accounts_accountID_order_replace": {
+        "url": "v3/accounts/{accountID}/orders",
+        "response": {
+            "orders": [
+                {
+                    "triggerCondition": "TRIGGER_DEFAULT",
+                    "replacesOrderID": "2125",
+                    "partialFill": "DEFAULT_FILL",
+                    "price": "1.25000",
+                    "createTime": "2016-10-05T10:52:43.742347417Z",
+                    "timeInForce": "GTC",
+                    "instrument": "EUR_USD",
+                    "state": "PENDING",
+                    "units": "-50000",
+                    "id": "2133",
+                    "type": "LIMIT",
+                    "positionFill": "POSITION_DEFAULT"
+                }
+            ],
+            "lastTransactionID": "2133"
+        }
     }
 }
 

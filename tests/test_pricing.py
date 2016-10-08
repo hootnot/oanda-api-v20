87db4c2c2b6b6efbe21a9ed65026e5a3e01cd849
hootnot <f.brekeveld@gmail.com>
2016-10-08 16:51:37 +0200

    streaming test modified, disconnect included

--- tests/test_pricing.py
+++ tests/test_pricing.py
@@ -66,20 +66,28 @@ class TestPricing(unittest.TestCase):
 
     @requests_mock.Mocker()
     def test__pricing_stream(self, mock_get):
-        """get the pricing information for instruments."""
+        """get the streaming pricing information for instruments."""
         uri = 'https://test.com/v3/accounts/{}/pricing/stream'.format(accountID)
-        resp = responses["_v3_accounts_accountID_pricing"]['response']
-        resp = {"a": 10}
-        text = json.dumps(resp)
+        ticks = [{"a": 10}, {"b": 20}, {"c": 30}, {"d": 40}, {"e": 50}]
+        text = "\n".join([json.dumps(r) for r in ticks])
         mock_get.register_uri('GET',
                               uri,
                               text=text)
         params = {"instruments": "EUR_USD,EUR_JPY"}
         r = pricing.PricingStream(accountID, params=params)
         result = []
+        n = 0
         for r in api.request(r):
-            result.append(r)
-        self.assertTrue(result[0] == resp)
+            result.append(json.dumps(r))
+            n += 1
+            # disconnect when we have 3 response lines
+            if n == 3:
+                api.disconnect()
+
+        # the result containing 3 items, should equal the first 3 items
+        # of the ticks
+        self.assertTrue("\n".join(result) ==
+                        "\n".join(json.dumps(r) for r in ticks[0:3]))
 
 
 if __name__ == "__main__":

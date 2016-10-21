2d591a23cee5d9f118acf652193f5c778abbca58
hootnot <f.brekeveld@gmail.com>
2016-10-21 09:02:26 +0200

    extended with start/end date

--- examples/candle-data.py
+++ examples/candle-data.py
@@ -15,8 +15,10 @@ granularities = sorted(granularities)
 # create the top-level parser
 parser = argparse.ArgumentParser(prog='candle-data')
 parser.add_argument('--nice', action='store_true', help='json indented')
-parser.add_argument('--count', default=5, type=int, help='num recs')
+parser.add_argument('--count', default=0, type=int, help='num recs, if not specified 500')
 parser.add_argument('--granularity', choices=granularities, required=True)
+parser.add_argument('--start', type=str, help="date-time YYYY-MM-DDTHH:MM:SS")
+parser.add_argument('--end', type=str, help="date-time YYYY-MM-DDTHH:MM:SS")
 parser.add_argument('--instruments', type=str, nargs='?',
                     action='append', help='instruments')
 
@@ -30,8 +32,15 @@ class Main(object):
     def main(self):
 
         if self.clargs.instruments:
-            params = {"count": self.clargs.count,
-                      "granularity": self.clargs.granularity}
+            params = {}
+            if self.clargs.granularity:
+                params.update({"granularity": self.clargs.granularity})
+            if self.clargs.count:
+                params.update({"count": self.clargs.count})
+            if self.clargs.start:
+                params.update({"from": self.clargs.start})
+            if self.clargs.end:
+                params.update({"to": self.clargs.end})
             for i in self.clargs.instruments:
                 r = instruments.InstrumentsCandles(instrument=i, params=params)
                 rv = self.api.request(r)
@@ -39,10 +48,6 @@ class Main(object):
                 if self.clargs.nice:
                     kw = {"indent": self.clargs.nice}
                 print("{}".format(json.dumps(rv, **kw)))
-
-            return
-
-        raise ValueError
 
 
 if __name__ == "__main__":
@@ -54,9 +59,7 @@ if __name__ == "__main__":
     try:
         m = Main(api=api, accountID=accountID, clargs=clargs)
         m.main()
-    except ValueError as e:
-        parser.parse_args(["--help"])
     except V20Error as v20e:
         print("ERROR {} {}".format(v20e.code, v20e.msg))
-    else:
+    except Exception as e:
         print("Unkown error: {}".format(e))

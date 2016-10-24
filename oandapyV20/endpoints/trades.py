fa05098cb42c21c84a4396752667ee91a855a793
hootnot <f.brekeveld@gmail.com>
2016-10-24 18:30:09 +0200

    responses separated from trades

--- oandapyV20/endpoints/trades.py
+++ oandapyV20/endpoints/trades.py
@@ -3,41 +3,7 @@
 from .apirequest import APIRequest
 from .decorators import dyndoc_insert, endpoint, abstractclass, extendargs
 from .definitions.trades import definitions    # flake8: noqa
-
-responses = {
-    "_v3_accounts_accountID_trades": {
-        "url": "v3/accounts/101-004-1435156-001/trades",
-        "response": {
-            "trades": [
-                {
-                    "financing": "-1.8516",
-                    "openTime": "2016-10-04T08:51:40.444453952Z",
-                    "price": "10581.5",
-                    "unrealizedPL": "250.0000",
-                    "realizedPL": "0.0000",
-                    "instrument": "DE30_EUR",
-                    "state": "OPEN",
-                    "initialUnits": "-10",
-                    "currentUnits": "-10",
-                    "id": "2123"
-                },
-                {
-                    "financing": "-1.8516",
-                    "openTime": "2016-10-04T08:51:40.214522674Z",
-                    "price": "10581.5",
-                    "unrealizedPL": "250.0000",
-                    "realizedPL": "0.0000",
-                    "instrument": "DE30_EUR",
-                    "state": "OPEN",
-                    "initialUnits": "-10",
-                    "currentUnits": "-10",
-                    "id": "2121"
-                }
-            ],
-            "lastTransactionID": "2124"
-        }
-    }
-}
+from .responses.trades import responses
 
 
 @abstractclass

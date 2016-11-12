a6c4636261a90a5e8f943df44c7ebf9ee4d6b4f4
hootnot <f.brekeveld@gmail.com>
2016-11-12 10:46:43 +0100

    Initial

---
+++ oandapyV20/contrib/requests/takeprofitorder.py
@@ -0,0 +1,74 @@
+# -*- encoding: utf-8 -*-
+
+from baserequest import BaseRequest
+import oandapyV20.definitions.orders as OD
+
+
+class TakeProfitOrderRequest(BaseRequest):
+    """create a TakeProfit OrderRequest.
+
+    TakeProfitOrderRequest is used to build the body for a TakeProfitOrder.
+    The body can be used to pass to the OrderCreate endpoint.
+    """
+
+    def __init__(self,
+                 tradeID,
+                 price,
+                 clientTradeID=None,
+                 timeInForce=OD.TimeInForce.GTC,
+                 gtdTime=None,
+                 clientExtensions=None):
+        """
+        Instantiate a TakeProfitOrderRequest.
+
+        Parameters
+        ----------
+
+        tradeID : string (required)
+            the tradeID of an existing trade
+
+        price: float (required)
+            the price indicating the target price to close the order.
+
+        Example
+        -------
+
+            >>> import json
+            >>> from oandapyV20 import API
+            >>> import oandapyV20.endpoints.orders as orders
+            >>> from oandapyV20.contrib.requests import TakeProfitOrderRequest
+            >>>
+            >>> accountID = "..."
+            >>> client = API(access_token=...)
+            >>> tpo = TakeProfitOrderRequest(orderID="1234",
+            >>>                              price=1.22)
+            >>> print(json.dumps(tpo.data, indent=4))
+            >>> r = orders.OrderCreate(accountID, data=tpo.data)
+            >>> rv = client.request(r)
+            >>> ...
+        """
+        super(TakeProfitOrderRequest, self).__init__()
+
+        # by default for a TAKE_PROFIT order
+        self._data.update({"type": OD.OrderType.TAKE_PROFIT})
+        self._data.update({"timeInForce": timeInForce})
+
+        # required
+        self._data.update({"tradeID": tradeID})
+        self._data.update({"price": "{:.5f}".format(float(price))})
+
+        # optional, but required if
+        self._data.update({"gtdTime": gtdTime})
+        if timeInForce == OD.TimeInForce.GTD and not gtdTime:
+            raise ValueError("gtdTime missing")
+
+        # optional
+        self._data.update({"clientExtensions": clientExtensions})
+
+    @property
+    def data(self):
+        """data property.
+
+        return the JSON order body
+        """
+        return dict({"order": super(TakeProfitOrderRequest, self).data})

1e5420ca5ce4999b828f10225aa2bcec1cdaf242
hootnot <f.brekeveld@gmail.com>
2017-06-28 11:44:27 +0200

    orderBook / positionBook endpoints [ci skip]

--- oandapyV20/endpoints/instruments.py
+++ oandapyV20/endpoints/instruments.py
@@ -70,3 +70,91 @@ class InstrumentsCandles(Instruments):
         """
         super(InstrumentsCandles, self).__init__(instrument)
         self.params = params
+
+
+@endpoint("v3/instruments/{instrument}/orderBook")
+class InstrumentsOrderBook(Instruments):
+    """Get orderbook data for a specified Instrument."""
+
+    @dyndoc_insert(responses)
+    def __init__(self, instrument, params=None):
+        """Instantiate an InstrumentsOrderBook request.
+
+        Parameters
+        ----------
+        instrument : string (required)
+            the instrument to fetch candle data for
+
+        params : dict
+            optional request query parameters, check developer.oanda.com
+            for details
+
+
+        Params example::
+
+            {_v3_instruments_instrument_orderbook_params}
+
+
+        OrderBook data example::
+
+        >>> import oandapyV20
+        >>> import oandapyV20.endpoints.instruments as instruments
+        >>> client = oandapyV20.API(access_token=...)
+        >>> params = ...
+        >>> r = instruments.InstrumentsOrderBook(instrument="DE30_EUR",
+        >>>                                      params=params)
+        >>> client.request(r)
+        >>> print r.response
+
+
+        Output::
+
+            {_v3_instruments_instrument_orderbook_resp}
+
+        """
+        super(InstrumentsOrderBook, self).__init__(instrument)
+        self.params = params
+
+
+@endpoint("v3/instruments/{instrument}/positionBook")
+class InstrumentsPositionBook(Instruments):
+    """Get positionbook data for a specified Instrument."""
+
+    @dyndoc_insert(responses)
+    def __init__(self, instrument, params=None):
+        """Instantiate an InstrumentsPositionBook request.
+
+        Parameters
+        ----------
+        instrument : string (required)
+            the instrument to fetch candle data for
+
+        params : dict
+            optional request query parameters, check developer.oanda.com
+            for details
+
+
+        Params example::
+
+            {_v3_instruments_instrument_positionbook_params}
+
+
+        PositionBook data example::
+
+        >>> import oandapyV20
+        >>> import oandapyV20.endpoints.instruments as instruments
+        >>> client = oandapyV20.API(access_token=...)
+        >>> params = ...
+        >>> r = instruments.InstrumentsPositionBook(instrument="DE30_EUR",
+        >>>                                         params=params)
+        >>> client.request(r)
+        >>> print r.response
+
+
+        Output::
+
+            {_v3_instruments_instrument_positionbook_resp}
+
+        """
+        super(InstrumentsPositionBook, self).__init__(instrument)
+        self.params = params

a65bf6c5d5b1842472975cbfe9697f6dec6765d2
hootnot <f.brekeveld@gmail.com>
2017-06-30 00:50:46 +0200

    initial

---
+++ tests/test_contrib_factories.py
@@ -0,0 +1,55 @@
+import sys
+import math
+import unittest
+import re
+
+try:
+    from nose_parameterized import parameterized
+except:
+    print("*** Please install 'nose_parameterized' to run these tests ***")
+    exit(0)
+
+
+import oandapyV20.contrib.factories as req
+
+
+class TestContribFactories(unittest.TestCase):
+    """Tests regarding contrib factories.
+
+    The reference is created using the second dict parameter. The
+    first dict parameter is merge with this, but only for the keys
+    that do NOT exist. That allows us to override parameters.
+    The result should reflect the data constructed by the class
+
+    """
+
+    @parameterized.expand([
+       (req.CandleHistoryRequest,
+           "DE30_EUR",
+           {"from": "2017-01-01T00:00:00Z",
+            "to": "2017-01-02T00:00:00Z",
+            "granularity": "M1"},
+           {"len": int(math.ceil(24*60.0 / 500))},
+        ),
+    ])
+    def test__candlehistory(self, factory, instrument, inpar, refpar, exc=None):
+        """candlehistoryfactory."""
+        if not exc:
+            # run the factory
+            i = 0
+            for r in factory(instrument, params=inpar):
+                if i == 0:
+                    self.assertTrue(r.params['from'] == inpar['from'])
+                if i == refpar['len']:
+                    self.assertTrue( 'to' not in r.params)
+                i += 1
+
+            self.assertTrue(i == refpar['len'])
+        else:
+            with self.assertRaises(exc) as err:
+                r = cls(**inpar)
+
+
+if __name__ == "__main__":
+
+    unittest.main()
